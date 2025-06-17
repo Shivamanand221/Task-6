5e99fb1f4b649a318007b0aa703f320a0dc3500c
Shivamanand221 <108640569+Shivamanand221@users.noreply.github.com>
2025-06-17 15:03:48 +0530

    Update ec2.tf

--- strapi-terraform/ec2.tf
+++ strapi-terraform/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_key_pair" "deployer" {
 
 # Security group allowing SSH and Strapi access
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
+  name        = "strapi_sg"
   description = "Allow SSH and Strapi access"
   vpc_id      = aws_vpc.main.id
 
@@ -55,3 +55,4 @@ resource "aws_instance" "strapi_ec2" {
     Name = "StrapiEC2"
   }
 }
+ 
