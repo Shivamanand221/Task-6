81172b0e08ab266e1e69d35a5728281e3ed0d04d
Shivamanand221 <shivamanand221@gmail.com>
2025-06-18 16:26:58 +0530

    Added files of Task-7

--- strapi-terraform/ecs.tf
+++ strapi-terraform/ecs.tf
@@ -21,7 +21,6 @@ resource "aws_ecs_task_definition" "strapi" {
   network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
-
   execution_role_arn = data.aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
