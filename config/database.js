223672be199eb9b940912ae61f2efd9fbe79f521
Shivamanand221 <shivamanand221@gmail.com>
2025-06-19 00:37:26 +0530

    Added files of Task-7

--- config/database.js
+++ config/database.js
@@ -1,45 +1,59 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = postgres;
+// module.exports = ({ env }) => {
+//   const client = postgres;
 
-  const connections = {
-    postgres: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
-    },
-  };
+//   const connections = {
+//     postgres: {
+//       connection: {
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         user: env('DATABASE_USERNAME', 'strapi'),
+//         password: env('DATABASE_PASSWORD', 'strapi'),
+//         ssl: env.bool('DATABASE_SSL', false) && {
+//           key: env('DATABASE_SSL_KEY', undefined),
+//           cert: env('DATABASE_SSL_CERT', undefined),
+//           ca: env('DATABASE_SSL_CA', undefined),
+//           capath: env('DATABASE_SSL_CAPATH', undefined),
+//           cipher: env('DATABASE_SSL_CIPHER', undefined),
+//           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+//         },
+//         schema: env('DATABASE_SCHEMA', 'public'),
+//       },
+//       pool: {
+//         min: env.int('DATABASE_POOL_MIN', 2),
+//         max: env.int('DATABASE_POOL_MAX', 10),
+//       },
+//     },
+//   };
 
   // console.log('Client value:', client);
   // console.log('Resolved config:', connections[client]);
-  if (!selected) {
-    console.error(`❌ Invalid DATABASE_CLIENT: ${client}`);
-    process.exit(1); // crash early with meaningful error
-  }
-
-  return {
+  module.exports = () => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...selected,
-      //acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      host: 'strapi_postgres',     // 🔁 Change this if needed (e.g., RDS endpoint)
+      port: 5432,
+      database: 'strapi',
+      user: 'strapi',
+      password: 'strapi',
+      ssl: false,
     },
-  };
-};
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+});
+
+
+  // return {
+  //   connection: {
+  //     client,
+  //     ...selected,
+  //     //acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+  //   },
+  // };
+
