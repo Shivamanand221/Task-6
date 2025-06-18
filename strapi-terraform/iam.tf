75c88e31b7ef4805b6c26eaf33a6f08d37a6fa0c
Shivamanand221 <shivamanand221@gmail.com>
2025-06-18 15:52:28 +0530

    Added files of Task-7

--- strapi-terraform/iam.tf
+++ strapi-terraform/iam.tf
@@ -1,18 +1,4 @@
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "ecsTaskExecutionRolePersonal"  # Changed name
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "ecs_task_policy_attach" {
-  role       = aws_iam_role.ecs_task_execution.name  # still correct
+  role       = data.aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
