f5c357d542f214d65dfe658211cf63d7df1bb388
Shivamanand221 <shivamanand221@gmail.com>
2025-06-18 11:38:53 +0530

    Pushed files of Task-7

---
+++ strapi-terraform/ecs.tf
@@ -0,0 +1,45 @@
+resource "aws_ecs_cluster" "strapi" {
+  name = "strapi-cluster"
+}
+
+resource "aws_ecs_task_definition" "strapi" {
+  family                   = "strapi-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = "${aws_ecr_repository.strapi.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 1337
+          hostPort      = 1337
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "strapi" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi.id
+  task_definition = aws_ecs_task_definition.strapi.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = [aws_subnet.public_subnet_1.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ecs_task_policy_attach
+  ]
+}
--- strapi-terraform/outputs.tf
+++ strapi-terraform/outputs.tf
@@ -1,6 +1,13 @@
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.strapi_ec2.public_ip
+# output "ec2_public_ip" {
+#   description = "Public IP of the EC2 instance"
+#   value       = aws_instance.strapi_ec2.public_ip
+# }
+
+
+output "strapi_task_arn" {
+  value = aws_ecs_task_definition.strapi.arn
 }
 
-
+output "strapi_service_name" {
+  value = aws_ecs_service.strapi.name
+}
