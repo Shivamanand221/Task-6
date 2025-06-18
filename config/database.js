a7e230bc652f74d5c0c5cc0f7a0c2571fa61dc07
Shivamanand221 <shivamanand221@gmail.com>
2025-06-18 21:12:00 +0530

    Added files of Task-7

--- config/database.js
+++ config/database.js
@@ -4,30 +4,9 @@ module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
-    mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
-    },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
+        // connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
@@ -48,16 +27,10 @@ module.exports = ({ env }) => {
         max: env.int('DATABASE_POOL_MAX', 10),
       },
     },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
   };
 
-  console.log('Client value:', client);
-  console.log('Resolved config:', connections[client]);
+  // console.log('Client value:', client);
+  // console.log('Resolved config:', connections[client]);
 
   return {
     connection: {
