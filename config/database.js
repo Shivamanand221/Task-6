0f7031c3aed43a652a0571b6dfc763ec0a03efa1
Shivamanand221 <shivamanand221@gmail.com>
2025-06-19 11:58:54 +0530

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -6,20 +6,12 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'strapi_postgres'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: false,
-        // && {
-          // key: env('DATABASE_SSL_KEY', undefined),
-          // cert: env('DATABASE_SSL_CERT', undefined),
-          // ca: env('DATABASE_SSL_CA', undefined),
-          // capath: env('DATABASE_SSL_CAPATH', undefined),
-          // cipher: env('DATABASE_SSL_CIPHER', undefined),
-          // rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        // },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: {
@@ -33,9 +25,6 @@ module.exports = ({ env }) => {
   if (!config) {
     throw new Error(`Database client "${client}" is not supported.`);
   }
-
-  console.log('✅ Loaded DATABASE_CLIENT:', client);
-  console.log('✅ Using connection config:', config);
 
   return {
     connection: {
