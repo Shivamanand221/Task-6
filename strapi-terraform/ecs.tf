4b29be585a5734f203c46c0d3e7f0d78c5f52798
Shivamanand221 <shivamanand221@gmail.com>
2025-06-20 13:14:17 +0530

    Added files of task-7

--- strapi-terraform/ecs.tf
+++ strapi-terraform/ecs.tf
@@ -112,13 +112,22 @@ resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi.id
   task_definition = aws_ecs_task_definition.strapi.arn
-  launch_type     = "FARGATE"
+  # launch_type     = "FARGATE"
   desired_count   = 1
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
 
   network_configuration {
     subnets          = [aws_subnet.public.id]
     assign_public_ip = true
     security_groups  = [aws_security_group.ecs.id]
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
   }
 
   depends_on = [
