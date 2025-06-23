a84aff8e16b3d110036b247af84ce814b0c23910
Shivamanand221 <shivamanand221@gmail.com>
2025-06-23 17:52:19 +0530

    Initial Commit

--- strapi-terraform/user-data.sh
+++ strapi-terraform/user-data.sh
@@ -4,6 +4,5 @@ yum install -y docker
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
-# Pull and run Strapi container 
 sudo docker pull shivamanand221/strapi:latest
 sudo docker run -d -p 1337:1337 shivamanand221/strapi:latest
