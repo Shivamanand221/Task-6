09dc6e8a0e306a82968b7aafd87c42918d6a8c19
Shivamanand221 <shivamanand221@gmail.com>
2025-06-23 12:14:27 +0530

    Task-10

--- strapi-terraform/ec2.tf
+++ strapi-terraform/ec2.tf
@@ -1,7 +1,7 @@
 # SSH key pair
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("${path.module}/deployer-key.pub")
+  public_key = var.deployer_public_key
 }
 
 # Security group allowing SSH and Strapi access
