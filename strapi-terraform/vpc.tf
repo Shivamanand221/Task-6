99657761e004e63ab89fae5ec4053a9d163c2672
Shivamanand221 <shivamanand221@gmail.com>
2025-06-23 16:01:39 +0530

    Task-10

--- strapi-terraform/vpc.tf
+++ strapi-terraform/vpc.tf
@@ -9,7 +9,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Create a public subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
