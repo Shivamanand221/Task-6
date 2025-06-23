30f0104e926320d8b14ffb44140a2edb2fdbec9c
Shivamanand221 <shivamanand221@gmail.com>
2025-06-23 13:24:21 +0530

    Task-10

--- strapi-terraform/iam.tf
+++ strapi-terraform/iam.tf
@@ -3,7 +3,6 @@
 #   name = "ecsTaskExecutionRolePersonal"
 # }
 
-
 # resource "aws_iam_role_policy_attachment" "ecs_task_policy_attach" {
 #   role       = data.aws_iam_role.ecs_task_execution.name
 #   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
